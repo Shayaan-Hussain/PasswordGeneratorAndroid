b477cc023c1fe911ac3cfce891df06075b556639
Shayaan-Hussain <shayaan.hussain2@gmail.com>
2021-01-11 23:47:44 +0530

    Code Upload

---
+++ app/src/main/java/com/shayaan/passwordgenerator/SplashScreen.kt
@@ -0,0 +1,17 @@
+package com.shayaan.passwordgenerator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+        Handler().postDelayed({
+            intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }, 1500)
+    }
+}
